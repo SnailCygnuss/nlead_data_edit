1e1d851e74bc7408182ebe75f2969813545c0067
Sahil Ahmed <sahil.a654@gmail.com>
2023-02-03 16:22:37 +0100

    Added all the files. Minimum example

---
+++ research_conf.js
@@ -0,0 +1,24 @@
+var dataSet = [
+[
+ '10th Euroma Sustainable Operations and Supply Chains Forum', 'Hamburg, Germany', '2023-03-20/23', '2022/10/30', '<a href="https://euroma-sustainabilityforum.org" target="_blank">Link</a>', '' ],
+[
+ '6th Interdisciplinary Conference on Production, Logistics and Traffic (ICPLT)', 'Dortmund, Germany', '2023-03-21/22', '2022/07/31', '<a href="https://itl.mb.tu-dortmund.de/veranstaltungsdetail/icplt-6th-interdisciplinary-conference-on-production-logistics-and-traffic-8942/" target="_blank">Link</a>', '' ],
+[
+ 'IPSERA 2023', 'Barcelona', '2023/04/02', '2022/12/16', '<a href="https://ipsera2023.com" target="_blank">Link</a>', '2-5 May 2023' ],
+[
+ 'ITS European Conference', 'Lisbon, Portugal', '2023-05-22/24', '2023/01/20', '<a href="https://itseuropeancongress.com" target="_blank">Link</a>', '' ],
+[
+ '13rd European Decision Sciences Conference', 'Nantes', '2023/06/04', '2023/01/20', '<a href="https://edsi.decisionsciences.org/conference/" target="_blank">Link</a>', '4-7 June' ],
+[
+ 'IPIC2023', 'Athens, Greece', '2023-06-13/15', '2023/01/27', '<a href="https://www.pi.events/IPIC2023/sites/default/files/IPIC2023_Call%20for%20Contributions%20Announcement.pdf" target="_blank">Link</a>', '' ],
+[
+ 'NOFOMA 2023', 'Helsinki', '2023/06/14', '2023/01/23', '<a href="http://nofoma2023.aalto.fi" target="_blank">Link</a>', '14-16 June 2023' ],
+[
+ 'WCTR', 'Montreal', '2023-07-17/24', '', '<a href="https://wctrs-society.com/world-conference/" target="_blank">Link</a>', '' ],
+[
+ '30th International Annual EurOMA Conference', 'Leuven', '2023/07/03', '2023/01/11', '<a href="https://euroma2023.org" target="_blank">Link</a>', 'Note: Deadline 12.00 CET' ],
+[
+ 'LRN Conference', 'Edinburgh', '2023-09-6/8', '2023/04/17', '<a href="https://ciltuk.org.uk/Events/National-Events-2023/Logistics-Research-Network-Conference/utm_source/GM/utm_medium/email/utm_content/85579?utm_campaign=LRN+2023+Call+for+papers+launch&utm_term=%7BEmailSubjectLine%7D&gator_td=JucXXHCmkVHVSPP5lsM2Yv4MEbKRtl%2fAUBUEEcrhhSvUPNwkZKpbe%2f1qWZrkdROSzFJcuE2%2bAE9%2fRiSOBGcJugs92FziWMCBilfCfczKMJx7UByZaCRrAN2Ts9YJWMSafXPHJD%2bk5DWtdyyLgexElha0%2fSJOWh14UuRr4RRrRw2jc7IUY7L4N%2f0Zttvjna9UBbuP48Vz0xhmLcAc1wqyVHwxWNFP76ctq%2bTTPEDf%2fEM%3d" target="_blank">Link</a>', '' ],
+[
+ 'EARPA Form Forum', 'Brussels', '2023-10-17/18', '', '<a href="https://www.earpa.eu/earpa/17/2287/earpa_form_forum___17___18_october_2023.html" target="_blank">Link</a>', '' ],
+];
