db031ae3df2e5fe93881e1cd45d3532614cc0c3f
Sahil Ahmed <sahil.a654@gmail.com>
2023-11-15 16:30:47 +0100

    Update Data

--- data/special_issues.js
+++ data/special_issues.js
@@ -16,6 +16,14 @@ var dataSet = [
 [
  'Annals of Operations Research', 'Springer', 'OR Applications for Resource Efficient and Climate Change Driven Issues in Digital Sustainable and Circular Supply Chains', '2023/12/31', '<a href="https://resource-cms.springernature.com/springer-cms/rest/v1/content/20203804/data/v2" target="_blank">Link</a>', '2022/09/07', '' ],
 [
+ 'International Journal of Lean Six Sigma', 'Emerald', 'Operational Excellence and Quality Improvement in Sweden', '2023/12/31', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/operational-excellence-and-quality-improvement-sweden" target="_blank">Link</a>', '2023/11/15', '' ],
+[
+ 'International Journal of Lean Six Sigma', 'Emerald', 'Integration of Lean Six Sigma with Industry 4.0 to Augment Organizational Efficiency and Effectiveness', '2023/12/31', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/integration-lean-six-sigma-industry-40-augment-organizational-efficiency-and" target="_blank">Link</a>', '2023/11/15', '' ],
+[
+ 'International Journal of Lean Six Sigma', 'Emerald', 'Operational Excellence Methodologies in Portuguese and Brazilian Organizations', '2023/12/31', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/operational-excellence-methodologies-portuguese-and-brazilian-organizations" target="_blank">Link</a>', '2023/11/15', '' ],
+[
+ 'Journal of Humanitarian Logistics and Supply Chain Management', 'Emerald', 'Emergency response logistics', '2023/12/31', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/emergency-response-logistics" target="_blank">Link</a>', '2023/11/15', '' ],
+[
  'International Journal of Production Economics', 'Elsevier', 'Data-driven Digital Transformation in Operations and Supply Chain Management', '2024/01/20', '<a href="https://www.sciencedirect.com/journal/international-journal-of-production-economics/about/call-for-papers" target="_blank">Link</a>', '2023/01/20', '' ],
 [
  'Journal of Purchasing & Supply Management', 'Elsevier', 'Purchasing and Supply Management Education', '2024/01/31', '<a href="https://www.sciencedirect.com/journal/journal-of-purchasing-and-supply-management/about/call-for-papers#purchasing-and-supply-management-education" target="_blank">Link</a>', '2023/03/31', '' ],
@@ -23,6 +31,8 @@ var dataSet = [
  'Journal of Supply Chain Management', 'Wiley', 'Radical innovations and extreme disruptions: How could a firm thrive from the coevolution of the two?', '2024/01/31', '<a href="https://onlinelibrary.wiley.com/pb-assets/assets/1745493X/JSCM%20SI%20CfP%20on%20Regeneration%20for%2060th%20Anniversary%20-%20final%20v2-1681317303413.pdf" target="_blank">Link</a>', '2023/03/31', '' ],
 [
  'International Journal of Physical Distribution & Logistics Management', 'Emerald', 'Platform-enabled supply chain and logistics excellence', '2024/02/01', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/platform-enabled-supply-chain-and-logistics-excellence" target="_blank">Link</a>', '2023/08/30', '' ],
+[
+ 'Journal of Knowledge Management', 'Emerald', 'Promoting knoweldge practices for stimulating innovation in Society 5.0: integrating humans and technology for building smarter and sustainable managerial perspectives', '2024/02/28', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/promoting-knoweldge-practices-stimulating-innovation-society-50-integrating-0" target="_blank">Link</a>', '2023/11/15', '' ],
 [
  'International Journal of Production Research', 'Taylor & Francis', 'Multistakeholder Production and Logistics Management through Blockchain Technology Applications', '2024/02/29', '<a href="https://www.callforpapers.co.uk/blockchain-applications" target="_blank">Link</a>', '2023/03/06', '' ],
 [
@@ -44,13 +54,9 @@ var dataSet = [
 [
  'Journal of Purchasing and Supply Management', 'Elsevier', 'Digitalization for Sustainable Purchasing and Supply Chain Management', '2024/03/31', '<a href="https://www.sciencedirect.com/journal/journal-of-purchasing-and-supply-management/about/call-for-papers" target="_blank">Link</a>', '2023/08/30', '' ],
 [
+ 'British Food Journal', 'Emerald', 'Re-designing short food supply chains for enhancing sustainable livelihoods and economies worldwide', '2024/05/01', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/re-designing-short-food-supply-chains-enhancing-sustainable-livelihoods-and" target="_blank">Link</a>', '2023/11/15', '' ],
+[
  'International Journal of Operations & Production Management', 'Emerald', 'Managing emergent uncertainties through adaptable supply chain planning', '2024/06/01', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/managing-emergent-uncertainties-through-adaptable-supply-chain-planning" target="_blank">Link</a>', '2023/11/03', '' ],
-[
- 'European Journal of Marketing', 'Emerald', 'Reinventing Marketing in a disruptive Economy', '2024/06/30', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/reinventing-marketing-a-disruptive-economy" target="_blank">Link</a>', '2023/11/03', '' ],
-[
- 'Transportation Research Part E: Logistics and Transportation Review', 'Elsevier', 'Intelligent Disaster Management: Integrated Emergency Logistics and Operations Systems', '2024/07/01', '<a href="https://www.sciencedirect.com/journal/transportation-research-part-e-logistics-and-transportation-review/about/call-for-papers#intelligent-disaster-management-integrated-emergency-logistics-and-operations-systems" target="_blank">Link</a>', '2023/08/30', '' ],
-[
- 'International Journal of Production Research', 'Taylor & Francis', 'Sustainable manufacturing using Zero Defect Manufacturing', '2024/09/01', '<a href="https://www.callforpapers.co.uk/ijpr-zdm" target="_blank">Link</a>', '2023/08/30', '' ],
 [
  'International Journal of Production Economics', 'Elsevier', 'Sustainable semi-conductor supply chain under industrial disruption: Risk and resilience concerns', '2024/06/01', '<a href="https://www.sciencedirect.com/journal/international-journal-of-production-economics/about/call-for-papers" target="_blank">Link</a>', '2023/08/30', '' ],
 [
@@ -58,13 +64,27 @@ var dataSet = [
 [
  'Transportation Research Part E: Logistics and Transportation Review', 'Elsevier', 'Learning based Solution Techniques for Optimization in Logistics and Transportation Systems', '2024/06/01', '<a href="https://www.sciencedirect.com/journal/transportation-research-part-e-logistics-and-transportation-review/about/call-for-papers" target="_blank">Link</a>', '2023/08/30', '' ],
 [
+ 'Measuring Business Excellence', 'Emerald', 'Measuring, monitoring and reporting Sustainable Development Goals performance', '2024/06/15', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/measuring-monitoring-and-reporting-sustainable-development-goals-performance" target="_blank">Link</a>', '2023/11/15', '' ],
+[
  'The International Journal of Logistics Management', 'Emerald', 'Women in supply chain and logistics under the Industry 4.0 and Industry 5.0 workplace environment', '2024/06/16', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/women-supply-chain-and-logistics-under-industry-40-and-industry-50-workplace" target="_blank">Link</a>', '2023/11/03', '' ],
 [
+ 'European Journal of Marketing', 'Emerald', 'Reinventing Marketing in a disruptive Economy', '2024/06/30', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/reinventing-marketing-a-disruptive-economy" target="_blank">Link</a>', '2023/11/03', '' ],
+[
+ 'British Food Journal', 'Emerald', 'Alternative Proteins: Balancing sustainability, innovation and acceptance in the food supply chain.', '2024/06/30', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/alternative-proteins-balancing-sustainability-innovation-and-acceptance-food" target="_blank">Link</a>', '2023/11/15', '' ],
+[
+ 'Transportation Research Part E: Logistics and Transportation Review', 'Elsevier', 'Intelligent Disaster Management: Integrated Emergency Logistics and Operations Systems', '2024/07/01', '<a href="https://www.sciencedirect.com/journal/transportation-research-part-e-logistics-and-transportation-review/about/call-for-papers#intelligent-disaster-management-integrated-emergency-logistics-and-operations-systems" target="_blank">Link</a>', '2023/08/30', '' ],
+[
  'The International Journal of Logistics Management', 'Emerald', 'Metaverse Adoption and Implementation in Logistics and Supply Chain Management: Challenges, Issues and Opportunities', '2024/07/31', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/metaverse-adoption-and-implementation-logistics-and-supply-chain-management" target="_blank">Link</a>', '2023/11/03', '' ],
+[
+ 'International Journal of Lean Six Sigma', 'Emerald', 'Operational Excellence in Africa', '2024/07/31', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/operational-excellence-africa" target="_blank">Link</a>', '2023/11/15', '' ],
+[
+ 'International Journal of Production Research', 'Taylor & Francis', 'Sustainable manufacturing using Zero Defect Manufacturing', '2024/09/01', '<a href="https://www.callforpapers.co.uk/ijpr-zdm" target="_blank">Link</a>', '2023/08/30', '' ],
 [
  'International Journal of Production Research', 'Taylor & Francis', 'Digital Twin Transition for Intelligent and Resilient Industrial Systems', '2024/09/30', '<a href="https://www.callforpapers.co.uk/digitaltwin" target="_blank">Link</a>', '2023/11/03', '' ],
 [
  'International Journal of Operations & Production Management', 'Emerald', 'On Sustainable Operations and Supply Chain Management: What about Diversity, Equity, and Inclusion?', '2024/09/30', '<a href="https://www.emeraldgrouppublishing.com/calls-for-papers/sustainable-operations-and-supply-chain-management-what-about-diversity-equity-and" target="_blank">Link</a>', '2023/11/03', '' ],
+[
+ 'International Journal of Production Research', 'Taylor & Francis', 'Additive Manufacturing for Operations and Supply Chain Management', '2024/11/15', '<a href="https://www.callforpapers.co.uk/additive-manufacturing" target="_blank">Link</a>', '2023/11/15', '' ],
 [
  'Journal of Business Logistics', 'Wiley', 'Technology Adoption and Implementation: Implications for Resilience and Sustainability', '2024/12/01', '<a href="https://onlinelibrary.wiley.com/pb-assets/assets/21581592/Tech%20Adoption%20%20Implemention%20CFP-1696437058423.pdf" target="_blank">Link</a>', '2023/11/03', '' ],
 ];
