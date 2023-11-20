b1ab794ac8c706dacfb32e83dcd0f6a40a9643aa
Sahil Ahmed <sahil.a654@gmail.com>
2023-11-20 08:43:34 +0100

    Update Data

--- data/seminar_webinar.js
+++ data/seminar_webinar.js
@@ -1,4 +1,12 @@
 var dataSet = [
 [
+ 'AoA Transport All researchers day', 'HGU', '2023/12/01', '<a href="" target="_blank">Link</a>', '' ],
+[
+ 'Drive Sweden Lunch and Learn', 'Online', '2023/12/08', '<a href="https://en-gb.eu.invajo.com/event/drivesweden/lunchlearnnationellsamverkanokartillganglighetavdataitransportsystemet///invite?utm_campaign=event_invitation&utm_source=email&utm_medium=email" target="_blank">Link</a>', '12.00-13.00' ],
+[
  'Shipping and Trade Alliance for Research (STAR) Forum', 'Online', '2024/05/22', '<a href="https://forms.office.com/Pages/ResponsePage.aspx?id=YPc3j4exgUm4L-Xcb1vPRJ1F201npS9JqL2uNve16xtUMTNMVE9QUFNRRzFRV0RYQlhZNzRYU0o5VS4u" target="_blank">Link</a>', 'Submit by 31 December 2023' ],
+[
+ 'Vinnova: Made in Sweden – så leder vi en hållbar omställning av vägtransporter för global konkurrenskraft', 'Stockholm', '2024/05/28', '<a href="https://www.vinnova.se/aktuellt/kalender/?lightbox=lightbox&type=calendar&url=%2Fkalenderhandelser%2F2024%2F05%2Fmade-in-sweden--sa-leder-vi-en-hallbar-omstallning-av-vagtransporter-for-global-konkurrenskraft%2F" target="_blank">Link</a>', '' ],
+[
+ 'Nordic HCT Conference', 'Göteborg + Online', '2024/10/09', '<a href="https://sv-se.eu.invajo.com/event/closer/nordichctconference2024" target="_blank">Link</a>', '' ],
 ];
